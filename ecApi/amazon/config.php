d0472aa37b81461f60e05261bc3db2ce958528d1
tony <tony56recruit@gmail.com>
2017-04-20 10:12:37 +0900

    masking

--- ecApi/amazon/config.php
+++ ecApi/amazon/config.php
@@ -1,6 +1,6 @@
 <?php
 //Enter your IDS
-define("Associate_tag", "tony56az-22"); // アソシエイトタグ
-define("Access_Key_ID", "AKIAIKDQUOX2ZCIMSRLA"); // アクセスキー
-define("Secret_Access_Key", "+RDaJOr2ni0cpFuO4LQ3aGyA59Bat1zYPWh21FyB"); // シークレットキー
+define("Associate_tag", "***"); // アソシエイトタグ
+define("Access_Key_ID", "***"); // アクセスキー
+define("Secret_Access_Key", "***"); // シークレットキー
 ?>
